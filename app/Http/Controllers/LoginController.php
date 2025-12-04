31d56e8606cdeb905b16b6d134bea8610cd8a4ff
LuzAneliCampos <152234994+Luz-Campos@users.noreply.github.com>
2025-12-04 14:00:57 -0600

    Controller Luz

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,7 +9,7 @@ class LoginController extends Controller
      */
     private array $user = [
         'email' => 'equipo1@gmail.com',
-        'password' => 'Equipo1-12345'
+        'password' => 'Equipo1-12345',
     ];
 
     public function login(string $email, string $password): bool
